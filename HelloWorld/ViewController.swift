eed5dd24d5153920f51501cdaae2518681b3d9ea
bearachute <sjourand@gmail.com>
2018-06-10 02:31:47 -0400

    Objective C IOS app first

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by skt on 6/9/18.
+//  Copyright © 2018 sktcode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func showMessage(sender: UIButton) {
+        let alertController = UIAlertController(title: "Welcome to My First App", message: "Hello World", preferredStyle: UIAlertControllerStyle.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
+    
+}
+
+
